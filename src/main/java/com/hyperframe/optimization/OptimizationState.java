6740cc90bff1dfb824281458a846205178349a64
letov7482-ui <letov7482@gmail.com>
2026-09-05 13:43:17 +0300

    Add OptimizationState enum for optimization states

---
+++ src/main/java/com/hyperframe/optimization/OptimizationState.java
@@ -0,0 +1,14 @@
+package com.hyperframe.optimization;
+
+public enum OptimizationState {
+
+    DISABLED,
+    READY,
+    ANALYZING,
+    TESTING,
+    APPLYING,
+    ACTIVE,
+    INEFFECTIVE,
+    ROLLED_BACK,
+    FAILED
+}
